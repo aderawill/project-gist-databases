f8eacd68f5174d191515a1a7cce194614ae4f7ad
William Adera <aderawill@gmail.com>
2018-10-31 01:16:08 +0000

    second-try

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,7 +1,6 @@
 import requests
-import sqlite3
 
-gist = """INSERT INTO gists (
+insert_gist = """INSERT INTO gists (
     "github_id", "html_url", "git_pull_url",
     "git_push_url", "commits_url",
     "forks_url", "public", "created_at",
@@ -12,10 +11,10 @@ gist = """INSERT INTO gists (
     :public, :created_at, :updated_at,
     :comments, :comments_url
 );"""
-
 url = 'https://api.github.com/users/{username}/gists'
+
+
 def import_gists_to_database(db, username, commit=True):
-    
     resp = requests.get(url.format(username=username))
     resp.raise_for_status()
 
@@ -34,6 +33,6 @@ def import_gists_to_database(db, username, commit=True):
             "comments": gist['comments'],
             "comments_url": gist['comments_url'],
         }
-        db.execute(gist, params)
+        db.execute(insert_gist, params)
         if commit:
             db.commit()
